c093ae43c40540fa9fb316161443eb10f0d62d6a
Brian Mocarski <bsm1875@rit.edu>
2025-04-07 12:48:01 -0400

    e start

---
+++ server/controllers/Domo.js
@@ -0,0 +1,47 @@
+const models = require('../models');
+
+const { Domo } = models;
+
+const makerPage = async (req, res) => res.render('app');
+
+const makeDomo = async (req, res) => {
+  const { name, age } = req.body;
+
+  if (!name || !age) {
+    return res.status(400).json({ error: 'Both name and age are required!' });
+  }
+
+  const domoData = {
+    name,
+    age,
+    owner: req.session.account._id,
+  };
+
+  try {
+    const newDomo = new Domo(domoData);
+    await newDomo.save();
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists!' });
+    }
+    return res.status(500).json({ error: 'An error occurred making domo!' });
+  }
+};
+
+const getDomos = async (req, res) => {
+  try {
+    const domos = await Domo.find({ owner: req.session.account._id }).select('name age').lean().exec();
+    return res.json({ domos });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Error retrieving domos!' });
+  }
+};
+
+module.exports = {
+  makerPage,
+  makeDomo,
+  getDomos,
+};
